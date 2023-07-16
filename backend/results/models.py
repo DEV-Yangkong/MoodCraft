644dea1507d06aee32120adf417ed7455fc9cfc3
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-07-16 15:21:32 +0900

    [FIX] 백엔드 파일 최신화

--- backend/results/models.py
+++ backend/results/models.py
@@ -10,8 +10,11 @@ class Result(models.Model):
         return self.drink_kind
 
 class ResultDetailModel(models.Model):
+    dosu = models.CharField(max_length=10, verbose_name="도수")
+    sugar = models.CharField(max_length=10, verbose_name="당도")
     drink_name = models.CharField(max_length = 15, verbose_name="술 이름")
     description = models.TextField(verbose_name="설명")
+    image = models.ImageField(blank = True, null = True, upload_to="uploads")
     before_result = models.ForeignKey(
         "results.Result",
         related_name="results",
