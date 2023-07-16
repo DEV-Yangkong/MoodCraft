1822bf50842fce2ffd8c4c0a82382e2a19490ccd
ryoonago <133100923+ryoonago@users.noreply.github.com>
2023-07-17 00:30:37 +0900

    add toss.js

--- release/js/toss.js
+++ release/js/toss.js
@@ -1,6 +1,6 @@
 
 
-let URL = window.location.href.substr(30);
+let URL = window.location.search;
 console.log(URL);
 
 const urlParams = new URLSearchParams(URL);
@@ -17,8 +17,34 @@ console.log(sugar);
 axios.get(
     'http://localhost:8000/api/v1/result/soju/detail/',
     {params:
-      {drink_kind: URL,
+      {drink_kind: drink_kind,
         dosu: dosu,
       sugar: sugar
     }}
   )
+
+
+
+
+
+
+  //url 복사하기
+function sharePage () {
+    const shareObject = {
+      title: '오늘의 술! - MoodCraft',
+      text: '오늘의 술! - MoodCraft',
+      url: 'http://127.0.0.1:5500/release/resultProduct.html'
+    };
+    if (navigator.share){
+      navigator
+      .share(shareObject)
+      .then(() => {
+        alert('공유가 완료되었습니다');
+      })
+      .catch((error) => {
+        alert('에러가 발생했습니다');
+      })
+    } else{
+      alert('페이지 공유를 지원하지 않습니다');
+    }
+  }
