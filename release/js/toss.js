65a15930fd9ddd441f689448579ce7b94ab444b8
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-07-17 14:22:49 +0900

    윤아 css 수정 오류발생 고쳐야함

--- release/js/toss.js
+++ release/js/toss.js
@@ -1,3 +1,8 @@
+// 페이지 이동 함수
+function goToNextPage(page) {
+  window.location.href = page;
+}
+
 import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";
 
 let URL = window.location.search;
@@ -24,17 +29,17 @@ function Result() {
     })
     .then((response) => {
       let drink_name = document.createElement("div");
-      drink_name.textContent = response.data[0].drink_name;
+      drink_name.textContent = response.data[1].drink_name;
       const result1 = document.getElementById("result1");
       result1.appendChild(drink_name);
 
       let image = document.createElement("img");
-      image.src = response.data[0].image_url;
+      image.src = response.data[1].image_url;
       const result2 = document.getElementById("result2");
       result2.appendChild(image);
 
       let description = document.createElement("div");
-      description.textContent = response.data[0].description;
+      description.textContent = response.data[1].description;
       const result3 = document.getElementById("result3");
       result3.appendChild(description);
     });
@@ -69,4 +74,41 @@ function sharePage() {
   } else {
     alert("페이지 공유를 지원하지 않습니다");
   }
+}
+
+// 윤아꺼에 추가한 자스 코드:
+
+//페이스북 공유창
+function shareOnFacebook() {
+  const url = "http://localhost:5500"; // 여기에 공유할 URL을 입력하세요
+  const shareURL = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+    url
+  )}`;
+  const width = 500; // 팝업 창의 가로 크기
+  const height = 300; // 팝업 창의 세로 크기
+  const left = window.screen.width / 2 - width / 2; // 화면 중앙에 위치
+  const top = window.screen.height / 2 - height / 2; // 화면 중앙에 위치
+
+  window.open(
+    shareURL,
+    "_blank",
+    `width=${width}, height=${height}, top=${top}, left=${left}`
+  );
+}
+
+//카카오톡 공유창
+function shareOnKakao() {
+  Kakao.Link.sendDefault({
+    objectType: "feed",
+    content: {
+      title: "카카오톡 공유하기 테스트",
+      description: "카카오톡 공유하기를 테스트해봅니다.",
+      imageUrl:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FxmWRS%2FbtsnFK2q2us%2FCiJP4Qiy9OkHbJOaTrJNs1%2Fimg.png",
+      link: {
+        webUrl: "www.moodcraft.shop",
+        mobileWebUrl: "www.moodcraft.shop",
+      },
+    },
+  });
 }
