cda6cbe20811e741d848c5bef948c565a2524a98
dev.yangkong@gmail.com <dev.yangkong@gmail.com>
2023-07-17 16:19:23 +0900

    [remove] develop folder

--- develop/backend/visiters/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Visiter
-
-class VisiterSerializer(ModelSerializer):
-    class Meta:
-        model = Visiter
-
-        fields = "__all__" 
